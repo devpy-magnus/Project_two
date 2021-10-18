59f8dab3c28c173f2e69b277091cadc36f4cbd3e
devpy-magnus <90931616+devpy-magnus@users.noreply.github.com>
2021-10-18 15:31:57 +0200

    pop script

--- AppTwo/models.py
+++ AppTwo/models.py
@@ -10,7 +10,7 @@ def __str__(self):
     return self.top_name
 
 class Webpage(models.Model):
-    topic= models.ForeignKey(Topic)
+    topic = models.ForeignKey(Topic)
     name= models.CharField(max_length=264, unique=True)
     url= models.URLField(unique=True)
 
@@ -19,7 +19,7 @@ def __str__(self):
 
 class AccessRecord(models.Model):
     name= models.ForeignKey(Webpage)
-    data= models.DateField()
+    date= models.DateField()
 
 def __str__(self):
     return str(self.date)
